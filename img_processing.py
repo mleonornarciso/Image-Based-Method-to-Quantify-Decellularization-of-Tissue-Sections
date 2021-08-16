c21a37ce1c2d66fb6a523757fc19a5716387a860
mleonornarciso <42115487+mleonornarciso@users.noreply.github.com>
2021-08-16 11:13:49 +0200

    Update img_processing.py

--- img_processing.py
+++ img_processing.py
@@ -6,66 +6,52 @@ from skimage.morphology import disk
 from skimage import exposure
 from skimage.filters import threshold_yen
 
+
 def rgb2gray(rgb_img):
 	"""
-	Convert image from rgb to greyscale. not universal, specific to this problem
+	Convert image from rgb to greyscale. 
 	"""
 	gray = (0.3*rgb_img[:,:,0])+(0.59*rgb_img[:,:,1])+(0.11*rgb_img[:,:,2])
 
 	return gray
 
-def average_intensity(masked_img, option, nb_pixels):
 
-	"""
-	Takes a dapi image as input, and returns the average intensity
-	Option 1: average pixel intensity
-	Option 2: median pixel intensity
-	Pixel intensity computed as square_root(channel_1^2 + channel_2^2 + channel_3^2)
-	Average intensity obtained by dividing total intensity by the number of pixels of interest
-	"""
+def eq_norm(img):
 	
-	#nb pixels=number of pixels of interest in the mask. Because a mask is 1s and 0s, 
-	#the number of pixels of interest is equal to its sum.
-	masked_img = rgb2gray(masked_img)
-	total_intensity = masked_img.sum()
-
-	if option == 1: #average
-		intensity=total_intensity/nb_pixels
-	elif option == 2: #median - fiz um masked array para rejeitar os zeros
-		m = np.ma.masked_equal(masked_img, 0)
-		intensity = np.ma.median(m)
-
-	return intensity, total_intensity, nb_pixels
-
-
-def find_mask_threshold(bf_img, lower_bound_1, upper_bound_1, lower_bound_2, upper_bound_2,width_filter):
-
 	"""
-	Define mask by converting image into rgb and setting all pixels with
-	values between the threshold to zero
-	inside this function the background is already corrected
+	Normalizes and equalized a grayscale image
+	shifts image to 0 to 1, increases contrast by setting the max to 1 and min to -1
+	eliminates outlier pixels, rescaling between 2 and 98% percentiles: increasing contrast 
 	"""
+	p2, p98 = np.percentile(img, (2, 98))
+	img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
+	img_eq_norm=img_rescale/img_rescale.max()
 
-	bf_img_gs = rgb2gray(bf_img)
-	bf_img_gs_corr=bg_correct(bf_img_gs)
-	mask =  np.logical_and(bf_img_gs_corr<upper_bound_1 , bf_img_gs_corr>lower_bound_1)
-	bf_img_gs_corr[mask] = 0 
-	bf_img_gs_corr[bf_img_gs_corr != 0] = 1
+	return img_eq_norm
 
-	img1=freq_filter(bf_img_gs_corr,width_filter)
-	#img_gray = np.mean(img_back, axis=1)
-	mask_a =  np.logical_and(img1<upper_bound_2 , img1>lower_bound_2)
-	img1[mask_a] = 1 
-	img1[img1 != 1] = 0
-	
-	return bf_img_gs_corr.astype(np.int),img1.astype(np.int)
+
+def entropy_filter (img):
+	"""
+	Applies an entropy filter to an image and thresolds it (yen thresholding)
+	Input: normalized and equalized image
+	Returns a masked image based on the entropy of the image.
+	"""
+	ent_img=entropy(img, disk(20))
+	thresh=threshold_yen(ent_img)
+	thresh_img = ent_img <= thresh
+	mask = invert_mask(thresh_img.astype(int))
+
+	return mask,ent_img
+
 
 def dil_er(mask,kernel,it):
+	
 	"""
-	Dilation followed by an erosion. Transforms the input into uint8
-	Useful to disconnect holes that are have been brought together so
+	it Dilations followed by an it+1 erosions. Transforms the input into uint8
+	Useful to disconnect holes that have been brought together so
 	the labeling and hole removal works better. 
 	"""
+	
 	mask_i= mask.astype(np.uint8)
 	kernel_dil = disk(kernel)
 	kernel_er = disk(kernel)
@@ -74,20 +60,23 @@ def dil_er(mask,kernel,it):
 
 	return erosion
 
-def invert_mask(mask):
-    out = np.ones(mask.shape)
-    out = out - mask
-    out[out==-1] = 1
-    
-    return out.astype(np.int)
 
 def mask_labeling(mask):
+	
+	"""
+	Uses the label function from scipy to categorize the different components according to pixel size.
+	"""		
     structure = np.ones((3, 3), dtype=np.int)
     labeled, ncomponents = label(mask, structure)
     return labeled, ncomponents
 
+
+
 def remove_small_components(labeled_mask,ncomponents,area):
-    
+
+	"""
+	Removes from the mask labeled components that have less pixels than ncomponents
+	"""
 	sizes=[]
 	labeled_mask_out = np.copy(labeled_mask)
 	for label in range(ncomponents):
@@ -101,50 +90,50 @@ def remove_small_components(labeled_mask,ncomponents,area):
 	return labeled_mask_out
 
 
+def average_intensity(masked_img, option, nb_pixels):
+
+	"""
+	Takes a dapi/uv image as input, and returns the average intensity
+	Option 1: mean pixel intensity
+	Option 2: median pixel intensity
+	Pixel intensity computed as square_root(channel_1^2 + channel_2^2 + channel_3^2)
+	Average intensity obtained by dividing total intensity by the number of pixels of interest
+	"""
+	
+	masked_img = rgb2gray(masked_img)
+	total_intensity = masked_img.sum()
+
+	if option == 1: #mean
+		intensity=total_intensity/nb_pixels
+	elif option == 2: #median
+		m = np.ma.masked_equal(masked_img, 0)
+		intensity = np.ma.median(m)
+
+	return intensity, total_intensity, nb_pixels
+
+
+def invert_mask(mask):
+	
+	"""
+	inverts a mask so the backgound becomes mask and the original mask becomes backgrounds.
+	useful for analysis of background pixels.
+	"""
+	
+    out = np.ones(mask.shape)
+    out = out - mask
+    out[out==-1] = 1
+    
+    return out.astype(np.int)
+
+
 def apply_mask2img(img, mask):
 
+	"""
+	Applies mask to image, where pixels outside the mask are equaled to zero.
+	"""
 	# create a rgb image (N,M,3) by repeating the mask for each channel
 	mask_rgb = np.zeros(img.shape,dtype=np.int)
 	for i in range(3):
 		mask_rgb[:,:,i] = mask
 	return img*mask_rgb # apply mask by multlipying it with image
 
-
-def eq_norm(img):
-	
-	#normalizes and equalized a grayscale image
-	#shifts image to 0 to 1, increases contrast by setting the max to 1 and min to -1
-	# eliminates outlier pixels, rescaling between 2 and 98% percentiles: increases contrast 
-	
-	p2, p98 = np.percentile(img, (2, 98))
-	img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
-	img_eq_norm=img_rescale/img_rescale.max()
-
-	return img_eq_norm
-
-
-def entropy_filter (img):
-	#input: normalized and equalized image
-	#returns a masked image based on the entropy of the image.
-
-	ent_img=entropy(img, disk(20))
-	thresh=threshold_yen(ent_img)
-	thresh_img = ent_img <= thresh
-	mask = invert_mask(thresh_img.astype(int))
-
-	return mask,ent_img
-
-
-def convertImage(img1):
-    
-    img = img1.convert("RGBA")
-  
-    datas = img.getdata()
-  
-    newData = []
-  
-    for items in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:
-            newData.append((255, 255, 255, 0))
-        else:
-            newData.append(item)
